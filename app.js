6e76f0397e6d2d7335429207767bd1c8f07e99c2
harshal211997 <harshal211997@gmail.com>
2025-04-18 09:53:55 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+async function firstSuccessfulPromise(promiseArray) {
+  for (let promise of promiseArray) {
+    try {
+      const result = await promise;
+      return result;
+    } catch (err) {
+      // Ignore the error and try the next one
+    }
+  }
+  return undefined;
+}
+
+// let promise = firstSuccessfulPromise([
+//   new Promise((resolve, reject) => reject()), 
+//   new Promise((resolve, reject) => resolve("Success!"))
+// ]);
+// promise.then(result => console.log(result)); // Should log "Success!"
+
+module.exports.firstSuccessfulPromise = firstSuccessfulPromise;
