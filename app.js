89ba9b4126f88aa9ea01107885b073d24c659f4d
harshal211997 <harshal211997@gmail.com>
2025-04-19 11:03:16 +0530

    changes a value to 25

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ async function firstSuccessfulPromise(promiseArray) {
       // Ignore the error and try the next one
 	  //JIRA02
 	  let str = 'Hii'
-	  let a =10;
+	  let a =25;
     }
   }
   return undefined;
